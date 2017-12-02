0afd682111a4ba32b029ef6e30efec02a85873a6
Volodymyr Kvashenko <vpkvashenko@gmail.com>
2017-12-02 19:45:34 +0200

    Added a QueuesExample.

---
+++ src/main/java/com/examples/queue/QueuesExample.java
@@ -0,0 +1,25 @@
+package com.examples.queue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+/**
+ * @author Volodymyr Kvashenko
+ * Created on 02.12.2017.
+ */
+public class QueuesExample {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new PriorityBlockingQueue<>();
+
+        new Thread(() -> {
+            try {
+                System.out.println(queue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> queue.offer("The element added to the queue")).start();
+    }
+}
